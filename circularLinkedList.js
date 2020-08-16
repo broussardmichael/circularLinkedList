740a0523856e26d8aef663cfb4105bd642009ac3
michaelbroussard <michael_broussard@msn.com>
2020-08-16 12:20:07 -0500

    looking to extend linked list here.

---
+++ circularLinkedList.js
@@ -0,0 +1,94 @@
+let mod = module.exports = (function(){
+    let module = require("../linkedList/linkedList");
+    let linkedList = module();
+
+    class CircularLinkedList {
+        constructor(head = null) {
+            this.head = head;
+            this.deleteNode = deleteNode;
+            this.getNodeCountFromList = getNodeCountFromList;
+            this.isTheNodeInTheListByData = isTheNodeInTheListByData;
+        }
+    }
+
+    let circularLinkedList = {}
+
+    function validateNodeArgumentForOperations(node) {
+        if (node == null)
+            throw "A node is required to perform operations."
+    }
+
+    function validateListArgumentForOperations(list) {
+        if (list.head == null)
+            throw "List hasn't been constructed. Build a list before work can be done."
+    }
+
+    function deleteNode(node){
+        let list = this;
+        validateNodeArgumentForOperations(node);
+        validateListArgumentForOperations(list);
+
+        if (!list.isTheNodeInTheListByData(node))
+            throw "Unable to delete node, the node was not found in the list by its data.";
+
+        if (list.getNodeCountFromList() === 1) {
+            list.head = null;
+        } else {
+            let nextNode = node.nextNode, prevNode = node.prevNode;
+            if (node.data === list.head.data) {
+                list.head = nextNode;
+            } else {
+                prevNode = node.prevNode;
+                prevNode.nextNode = nextNode;
+                nextNode.prevNode = prevNode;
+            }
+        }
+        node = null;
+        delete node;
+    }
+
+    function getNodeCountFromList(){
+        let list = this;
+        validateListArgumentForOperations(list);
+
+        let node = list.head;
+        let count = 1;
+        while (node.nextNode.data !== list.head.data) {
+            node = node.nextNode;
+            count++;
+        }
+        return count;
+    }
+
+    function isTheNodeInTheListByData(node){
+        let list = this, found = false;
+        let currentNode = list.head;
+
+        validateNodeArgumentForOperations(node);
+        validateListArgumentForOperations(list);
+
+        let listLengthByNodes = list.getNodeCountFromList();
+
+        for(let i = 0; i < listLengthByNodes; i++) {
+            if (currentNode.data === node.data) {
+                found = true;
+                break;
+            } else {
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+        return found
+    }
+
+    circularLinkedList.createCircularLinkedListFromDataArray = function (dataArray){
+        let list = linkedList.createLinkedListFromDataArray(dataArray);
+        let tailNode = list.getTailNode();
+        list.head.prevNode = tailNode;
+        tailNode.nextNode = list.head;
+
+        circularLinkedList = new CircularLinkedList(list.head);
+        return circularLinkedList;
+    }
+    return circularLinkedList;
+});
